31648412cc9885eef9b1e754f17a2fc99bbdf519
tanayg <tgottigundala@gmail.com>
2018-07-14 17:40:48 -0700

    Sorting by release date

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import config
 import spotify
 
 def main():
-    results = spotify.getArtistData("RL Grime")
-    print results
+    results = spotify.getArtistData(config.artists["RL Grime"])
+    print results["items"][0]["release_date"]
 
 
 if __name__ == "__main__":
--- spotify.py
+++ spotify.py
@@ -2,8 +2,9 @@ import config
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def getArtistData(artistName):
+def getArtistData(artistId):
     client_credentials_manager = SpotifyClientCredentials(config.spotify["clientId"], config.spotify["clientSecret"])
     spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    results = spotify.search(q='artist:' + artistName, type='artist')
+    #results = spotify.artist(artistId)
+    results = spotify.artist_albums(artistId)
     return results
