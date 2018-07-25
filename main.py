6aa9d71ac03de8915a72e4258a7bdc800c1065d6
tanayg <tgottigundala@gmail.com>
2018-07-25 16:40:23 -0700

    Wrote a scheduler but still need to implement it

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ from spotify import *
 from reddit import *
 
 def main():
-    #Setup
-    artist = "RL Grime"
+    #sleepUntil("59:25")
+    job()
+
+def sleepUntil(checkTime):
+    currTime = datetime.datetime.now().time().strftime('%M:%S')
+    minDiff = int(checkTime[:2]) - int(currTime[:2])
+    secDiff = int(checkTime[3:]) - int(currTime[3:])
+    if (secDiff < 0):
+        minDiff -= 1
+        secDiff = 60 + secDiff
+    if (minDiff < 0):
+        minDiff = 60 + minDiff
+    print "Sleeping for " + str(minDiff) + ":" + str(secDiff)
+    time.sleep(minDiff * 60 + secDiff)
+    print "I'm awake boiiii"
+
+
+def job():
+    artist = "Kasbo"
+    print "Looking for new music by " + artist + "!"
 
     spotifyObj = Spotify(config.spotifyConfig["clientId"], config.spotifyConfig["clientSecret"])
 
@@ -20,23 +38,30 @@ def main():
     i = 0
     results = spotifyObj.getNewestTrackOrAlbum(config.artists[artist])
     print "Last Release: (" + results[2] + ") Today: (" + datetime.datetime.today().strftime('%Y-%m-%d') + ")"
+
+    newTrackRelease = True
+
     while results[2] < datetime.datetime.today().strftime('%Y-%m-%d'):
         try:
             results = spotifyObj.getNewestTrackOrAlbum(config.artists[artist])
-            print "Call " + str(i) + ", Time: " + datetime.datetime.now().time().strftime('%H:%M:%S')
+            currTime = datetime.datetime.now().time().strftime('%H:%M:%S')
+            print "Call " + str(i) + ", Time: " + currTime
             i = i + 1
+            if (time.time() - startTime) >= 60:
+                newTrackRelease = False
+                break
             time.sleep(1)
         except requests.exceptions.ConnectionError:
             print "Connection refused. Trying again"
             time.sleep(4)
             pass
-    postResult = redditObj.createPost(results[0], results[1], results[3])
+    #postResult = redditObj.createPost(results[0], results[1], results[3])
 
-    endTime = time.time()
-
-    print "Time: " + str(endTime - startTime)
-    print "Fetched from Spotify! Song Info: " + results[0] + ", " + results[1] + ", " + results[2] + ", " + results[3]
-    print "Posted to Reddit! Post Link: " + postResult
+    if newTrackRelease:
+        print "Fetched from Spotify! Song Info: " + results[0] + ", " + results[1] + ", " + results[2] + ", " + results[3]
+        #print "Posted to Reddit! Post Link: " + postResult
+    else:
+        print "No new release from " + artist + " :("
 
 
 if __name__ == "__main__":
